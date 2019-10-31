9ab858a5756825081c6e961415d6e1268ec7eb48
macorios22 <mark0android90@gmail.com>
2019-10-30 23:18:36 -0600

    Vista base y listado de las recetas existentes

--- rereceta/views.py
+++ rereceta/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+from .models import Receta, Categoria,Image
+from extra_views import CreateWithInlinesView, UpdateWithInlinesView, InlineFormSetFactory
 
-# Create your views here.
+
+class RecetaListView(generic.ListView):
+    model = Receta
+    template_name = "list.html"
+
+
+class RecetaDetailView(generic.DetailView):
+    model = Receta
+    template_name = "detail.html"
+
+
+class RecetaCreateView(generic.CreateView):
+    model = Receta
+    fields = '__all__'
+    template_name = "form.html"
+
+
+class RecetaUpdateView(generic.UpdateView):
+    model = Receta
+    fields = '__all__'
+    template_name = "form.html"
+
+
+class RecetaDeleteView(generic.DeleteView):
+    model = Receta
+    template_name = "delete.html"
+    success_url = reverse_lazy('rereceta:index')
+
+
+class CategoriaListView(generic.ListView):
+    model = Categoria
+    template_name = "categoria-list.html"
+
+
+class CategoriaDetailView(generic.DetailView):
+    model = Categoria
+    template_name = "categoria-detail.html"
+
+
+class CategoriaCreateView(CreateWithInlinesView):
+    model = Categoria
+    #inlines = [DetalleInLine]
+    fields = '__all__'
+    template_name = "categoria-form.html"
+
+
+class CategoriaUpdateView(UpdateWithInlinesView):
+    model = Categoria
+    #inlines = [DetalleInLine]
+    fields = '__all__'
+    template_name = "categoria-form.html"
+
+
+class CategoriaDeleteView(generic.DeleteView):
+    model = Categoria
+    template_name = "categoria-delete.html"
+    success_url = reverse_lazy('rereceta:categoria-index')
